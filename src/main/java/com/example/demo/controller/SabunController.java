9aa8f4eb0623aebd3a2fcdd14ff919e7516063c2
zerois-park <zerois.park@kt.com>
2024-11-18 16:27:51 +0900

    Update SabunController.java
    수정

--- src/main/java/com/example/demo/controller/SabunController.java
+++ src/main/java/com/example/demo/controller/SabunController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Sabuncontroller {
+public class SabunController {
 
     @GetMapping("/api/v1/user")
     public String getUserID() {
